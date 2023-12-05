aeeedeba8915c8ce07160c8876720494ffa814b8
khaled hazime <hazimekhaled01@gmail.com>
2023-12-05 09:16:14 -0300

    Update build-workspace.sh script with new dependencies and requirements

--- build-workspace.sh
+++ build-workspace.sh
@@ -8,14 +8,12 @@ BIWhite='\033[1;97m'      # White
 Off='\033[0m'             # Text Reset
 
 echo -e "${BIBlue}This script will build the catkin workspace for hera_robot.${Off}"
-# Check if script is running with sudo
-if [ "$EUID" -ne 0 ]
-  then echo -e "${BIRed}Please run this script with sudo!!${Off}"
+# Don't let user run this script as root
+if [ "$EUID" -eq 0 ]; then
+  echo -e "${BIRed}Please run this script as a normal user, not sudo.${Off}"
   exit
 fi
 
-# Set the user's home directory
-HOME_DIR=/home/"$SUDO_USER"
 
 # Check if ROS Noetic is installed
 if ! dpkg -s ros-noetic-desktop-full > /dev/null 2>&1; then
@@ -36,32 +34,47 @@ fi
 # Source ROS
 source /opt/ros/noetic/setup."$SHELL"
 
+# Set the user's home directory
+HOME_DIR=/home/"$SUDO_USER"
+# Set user's workspace directory
+WORKSPACE_DIR=$HOME_DIR/catkin_ws
+
 # Prompt user to enter GitHub personal access token
 echo ""
 echo -e "${BIBlue}Please enter your GitHub personal access token:${Off}"
 # shellcheck disable=SC2162
 read -s TOKEN
 
+# Prompt user to enter GitHub username
+echo ""
+echo -e "${BIBlue}Please enter your GitHub username:${Off}"
+# shellcheck disable=SC2162
+read USERNAME
+
 # Create the catkin workspace
 echo ""
 echo -e "${BICyan}Creating catkin workspace...${Off}"
-sudo -u "$SUDO_USER" mkdir -p $HOME_DIR/catkin_ws/src && cd $HOME_DIR/catkin_ws || exit 1
+mkdir -p $HOME_DIR/catkin_ws/src && cd $HOME_DIR/catkin_ws || exit 1
 catkin_make
 
 # Source the workspace
-source devel/setup."$SHELL"
+source $WORKSPACE_DIR/devel/setup."$SHELL"
 
 # Clone the repositories from hera_robot
-cd $HOME_DIR/catkin_ws/src || exit 1
-sudo -u "$SUDO_USER" mkdir -p hera_robot && cd hera_robot || exit 1
+cd $WORKSPACE_DIR/src || exit 1
+mkdir -p hera_robot && cd hera_robot || exit 1
 
 # List of repositories to clone
 REPOS=(
   "robofeiathome/hera"
   "robofeiathome/hera_control"
   "robofeiathome/hera_description"
+  "robofeiathome/hera_lowlevel"
+  "robofeiathome/hera_nav"
+  "robofeiathome/hera_simulation"
   "robofeiathome/hera_tasks"
   "robofeiathome/hera_telegram"
+  "robofeiathome/hera_speech"
   "robofeiathome/hera_moveit_config"
   "robofeiathome/hera_scripts"
 )
@@ -69,30 +82,34 @@ REPOS=(
 # Clone the repositories
 echo -e "${BIWhite}Cloning hera_robot repositories...${Off}"
 for REPO in "${REPOS[@]}"; do
-  git clone "https://$TOKEN:x-oauth-basic@github.com/$REPO.git"
+  git clone "https://$USERNAME:$TOKEN@github.com/$REPO.git"
 done
 
 # Clone 3rd party repositories
 echo -e "${BIWhite}Cloning 3rd party repositories...${Off}"
-cd $HOME_DIR/catkin_ws/src || exit 1
-sudo -u "$SUDO_USER" mkdir -p 3rd_party && cd 3rd_party || exit 1
+cd $WORKSPACE_DIR/src || exit 1
+mkdir -p 3rd_party && cd 3rd_party || exit 1
 
 # List of repositories to clone
 REPOS=(
   "robofeiathome/hera_agent"
   "robofeiathome/robot_resources"
   "ros-drivers/hokuyo_node"
-  "fagnerpimentel/map_server"
+  "robofeiathome/map_server"
   "fagnerpimentel/social_move_base"
   "fagnerpimentel/social_msgs"
   "robofei-home/speech_recognition"
-  "robofei-home/vision_system"
+  "robofeiathome/detector_2d"
+  "robofeiathome/hera_face"
+  "robofeiathome/hera_objects"
+  "robofeiathome/posture_analysis"
+  "robofeiathome/track_flow"
 )
 
 # Clone the repositories
 echo -e "${BIWhite}Cloning 3rd party repositories...${Off}"
 for REPO in "${REPOS[@]}"; do
-  git clone "https://$TOKEN:x-oauth-basic@github.com/$REPO.git"
+  git clone "https://$USERNAME:$TOKEN@github.com/$REPO.git"
 done
 
 # Get the dependencies from hera/dependencies.txt
@@ -100,12 +117,12 @@ echo -e "${BICyan}Getting dependencies...${Off}"
 DEPENDENCIES=()
 while IFS= read -r LINE; do
   DEPENDENCIES+=("$LINE")
-done < $HOME_DIR/catkin_ws/src/hera_robot/hera_scripts/dependencies.txt
+done < $WORKSPACE_DIR/src/hera_robot/hera_scripts/dependencies.txt
 
 # Install dependencies
 echo -e "${BIBlue}Installing dependencies...${Off}"
 for DEPENDENCY in "${DEPENDENCIES[@]}"; do
-  apt-get install -y "$DEPENDENCY"
+  sudo apt-get install -y "$DEPENDENCY"
 done
 
 # Get the python dependencies from hera/requirements.txt
@@ -113,7 +130,7 @@ echo -e "${BICyan}Getting python dependencies...${Off}"
 PYTHON_DEPENDENCIES=()
 while IFS= read -r LINE; do
   PYTHON_DEPENDENCIES+=("$LINE")
-done < $HOME_DIR/catkin_ws/src/hera_robot/hera_scripts/requirements.txt
+done < $WORKSPACE_DIR/src/hera_robot/hera_scripts/requirements.txt
 
 # Install python dependencies
 echo -e "${BIBlue}Installing python dependencies...${Off}"
